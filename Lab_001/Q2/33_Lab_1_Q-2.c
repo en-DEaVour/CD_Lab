5d2342c826c49d53dd599471692a6a4c53915d91
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2021-03-05 23:39:10 +0530

    Add files via upload

---
+++ Lab_001/Q2/33_Lab_1_Q-2.c
@@ -0,0 +1,27 @@
+
+
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+   char keyword[32][10]={
+      "auto","double","int","struct","break","else","long",
+      "switch","case","enum","register","typedef","char",
+      "extern","return","union","const","float","short",
+      "unsigned","continue","for","signed","void","default",
+      "goto","sizeof","voltile","do","if","static","while"
+   } ;
+   char input[10];
+   printf("Give input: ");
+   scanf("%s",input);
+   int flag=0,i;
+   for(i = 0; i < 32; i++) {
+      if(strcmp(input,keyword[i])==0) {
+         flag=1;
+      }
+   }
+   if(flag==1)
+      printf("-> %s is a keyword",input);
+   else
+      printf("-> %s is not a keyword",input);
+}
