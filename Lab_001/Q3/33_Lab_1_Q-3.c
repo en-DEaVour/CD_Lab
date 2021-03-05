8a83c76cdbf5486974e398b2a01f628b6c76a4df
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2021-03-05 23:41:40 +0530

    Add files via upload

---
+++ Lab_001/Q3/33_Lab_1_Q-3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+int i,flag=0,j=0;
+
+char s[100][10]={"+","-","*","/","++","--","=","==","!=","<","<=",">",">=","!","&&","||","%","^"};
+char t[100][20]={"addition","subtraction","multiplication","division","increment","decrement","assign",
+"equalto","! equal to","lessthan","lessthan equalto","greater than","greaterthan equalto","not","AND","OR",
+"module","power"};
+
+char w[10];
+printf("Enter a operator:");
+gets(w);
+
+	for(i=0;i<100;i++)
+    {
+          if(strcmp(w,s[i])==0)
+          {
+          		flag=1;
+          		j=i;
+          }
+	}	
+	if(flag==1)
+	{
+		printf("%s is a %s operator",w,t[j]);
+	}
+	else
+	{
+	 	printf("Choosen operator is not an Operator");
+	}
+}
+
+
