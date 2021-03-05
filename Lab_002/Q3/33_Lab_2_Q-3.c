dc12222e6dec97a4647fd5ea3112d2b0bdaae884
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2021-03-05 23:34:38 +0530

    Add files via upload

---
+++ Lab_002/Q3/33_Lab_2_Q-3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+    char number[10];
+    int flag = 0;
+    int length, i = 0;
+
+    printf("\n\nEnter a number: ");
+    scanf("%s", number);
+
+    length = strlen(number);
+    while(number[i++] != '\0')    
+    {
+        if(number[i] == '.')    
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag)
+        printf("\n\nEntered number is a Floating point Number\n\n");
+    else
+    printf("\n\nEntered number is a integer Number\n\n");
+    return 1;
+}
